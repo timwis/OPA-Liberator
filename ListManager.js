b918e1e3f5c7b4d92836c5e01ae8e804925409c5
Adam Hinz <hinz.adam@gmail.com>
2011-05-03 10:03:15 -0400

    updates to look and feel

---
+++ ListManager.js
@@ -0,0 +1,56 @@
+   var ListManager = Class.extend({
+	init: function(elem, uchange) {
+	    this.elem = elem;
+	    this.uchange = uchange;
+
+	    $(function() {
+		$( elem ).change(function() {
+		    var selected = [];
+		    //TODO: There has go to be a better way to do this...
+		    $(elem).children("option:selected").each(function() {
+			selected.push($(this).text()); 
+		    });
+
+		    if (selected != null && selected.length > 0) {
+			uchange(selected);
+		    }
+		}).change();	
+    });
+
+	    this.objects = [];
+	},
+
+	setObjects: function(objs) {
+	    this.objects = objs;
+	    this.render();
+	},
+
+	addObject: function(obj) {
+	    this.objects.push(obj);
+	    this.render();
+	    this.select(obj);
+	},
+
+	select: function(obj) {
+	    $(this.elem).children("option").each(function() {
+		q = $(this)
+		q.removeAttr("selected");
+		if (q.text() == obj) {
+		    q.attr('selected','selected');
+		}
+	    });
+
+	    this.uchange([obj]);
+	},
+
+	removeObject: function(obj) {
+	    this.objects.remove(obj);
+	    this.render();
+	},
+
+	render: function() {
+	    $(this.elem).html($.map(this.objects, function ( item ) { 
+		return "<option>" + item + "</option>"
+	    }).join('\n'));
+	}
+    });
---
+++ main.js
@@ -0,0 +1,72 @@
+function requestProperties(who) {
+    $.ajax({
+	url: "http://opa.timwis.com/fetch.php",
+	dataType: "jsonp",
+	data: {
+	    owner: who,
+	    exact: "true"
+	},
+	success: function( d ) { $.publish("/ajax/address/update", [d]); }
+    });
+}
+
+
+var l = new ListManager("#properties", function () {});
+
+// Wire up events...
+$.subscribe("/ajax/address/update", function (objs) {
+    l.setObjects($.map(objs, function( obj ) {
+	return obj.address;
+    }));
+});
+
+$(function() {
+    $( "#properties" ).change(function() {
+	$( "#properties" ).children("option:selected").each(function() {
+	    $.publish("/local/address/select", [$(this).text()]);
+	});
+    }).change();
+});
+
+
+	$(function() {
+
+	    $( "#owner" ).keyup(function( event ) {
+		if(event.keyCode == 13){
+		    requestProperties($( "#owner" ).val());
+		}
+	    });
+
+
+	    $( "#owner" ).autocomplete({
+		source: function( request, response ) {
+		    $.ajax({
+			url: "http://opa.timwis.com/fetch.php", 
+			dataType: "jsonp",
+			data: {
+			    owner: request.term
+			},
+			success: function( data ) {
+			    response( $.map( data, function( item ) {
+				return {
+				    label: item.owner, 
+				    value: item.oweer
+				}
+			    }));
+			}
+		    });
+		},
+		minLength: 2,
+		select: function( event, ui ) {
+		    if (ui.item) {
+			//m.addObject(ui.item.label);
+		    }
+		},
+		open: function() {
+		    $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+		},
+		close: function() {
+		    $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+		}
+	    });
+	});

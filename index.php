63c30bdea5acf8828383cb9b89b564fdd616d769
Adam Hinz <hinz.adam@gmail.com>
2011-06-19 17:31:40 -0400

    add google analytics

--- index.php
+++ index.php
@@ -29,6 +29,20 @@ function printfile($filename) {
     <script src="js/maps.js"></script>
 
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> 
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-24065525-1']);
+_gaq.push(['_setDomainName', '.phillyaddress.com']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
+
+</script>
 </head> 
 <body>
   <br />
